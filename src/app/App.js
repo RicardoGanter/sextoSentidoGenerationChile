80cc15740a66df7fad47378f0608cfb1fbe88707
Ricardo Ganter <ricardorosalesganter@gmail.com>
2024-06-20 21:46:26 -0400

    estructura ya creada con react

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+ 
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">  
+        <h1>asdas</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
