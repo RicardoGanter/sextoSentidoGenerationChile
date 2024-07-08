d324ff2bb70a79a10fe2474c3d30f748c03a8b00
Ricardo Ganter <ricardorosalesganter@gmail.com>
2024-07-08 14:33:48 -0400

    nuevo desing de login, falta la logica de js todavia, y utilizar buenas practicas

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -1,25 +1,72 @@
 import { Link } from "react-router-dom";
-
+import styles from "./../../styles/login.module.css"
+import logoImg from "../../img/Logo1.png"
+import logo2Img from "../../img/2.png"
+import mailIcon from "../../img/envelope-solid.svg"
+import passwordIcon from "../../img/lock-solid.svg"
+import { useState } from "react";
 const Login = ()=>{
-    
+    const [email, setEmail] = useState() 
+    const [password, setPassword] = useState() 
+    function validarCorreo(email) { 
+        const correctEmail = /^[a-zA-Z0-9._-]+@gmail\.com$/; 
+        return correctEmail.test(email);
+    }
+    const logIn = (event) => { 
+        event.preventDefault()
+        if (validarCorreo(email)){
+            console.log(email)
+        }
+        else{
+            alert("Tiene que ingresar un correo con Gmail")
+        }
+    }
     return (
-        <form action="submit_form.php" method="post" className={"containLogin"}>
-            <div className={"containInput"}>
-                <label htmlFor="nombre">Rut</label>
-                <input placeholder="12.345.678-9" type="text" id="nombre" name="nombre" required/>
-            </div> 
-            <div className={"containInput"}  >
-                <label htmlFor="password">Contraseña</label>
-                <input  type="password" id="password" name="password" required/>
+        <div className={styles.contain}>
+            <div className={styles.containLogos}>
+                <img alt="Logo" src={logoImg} width={350}/>
+                <img alt="Logo" src={logo2Img} width={350}/>
+                <p>
+                    “Equilibra tu cuerpo, revitaliza tu vida”
+                </p>
             </div>
-            <div className={"containLinks"}>
-                <a href="k">¿Has olvidado tu contraseña?</a>
-                <a href="k">Crear cuenta</a>
+            <div>
+                
+                <form onSubmit={logIn} method="post" className={styles.containLogin}>
+                    <h1>¡Bienvenido de nuevo!</h1>
+                    <div className={styles.containInput}> 
+                        <label htmlFor="email">Correo electronico</label>
+                        <div>
+                            <img alt="IconEmail" className={styles.iconInput} src={mailIcon}/>
+                            <input className={styles.input} placeholder="Correo electronico" type="email" value={email} id="email" name="email" onChange={req => setEmail(req.target.value)} required/>
+                        </div>
+                        
+                    </div> 
+                    <div className={styles.containInput}  >
+                        <label htmlFor="password">Contraseña</label>
+                        <div>
+                            <img alt="IconContraseña" className={styles.iconInput} src={passwordIcon}/>
+                            <input className={styles.input} placeholder="Contraseña" type="password" id="password" name="password" value={password} onChange={req => setPassword(req.target.value)} required/>
+                        </div>
+                        
+                    </div>
+                    {/* <Link to={'/user/home'}> */}
+                        <button type="submit"  className={styles.buttonEnter}>Iniciar sesión</button>
+                    {/* </Link> */}
+                    <div className={styles.containLinks}>
+                        <Link className={styles.enlaces} href=""> ¿Eres un profesional? Iniciar sesión</Link>
+                        <Link className={styles.enlaces} href="k">¿Has olvidado tu contraseña?</Link>
+                        <Link className={styles.enlaces} href="k">¿Todavía no tienes una cuenta? Regístrate</Link>
+                    </div>
+                        
+                </form> 
             </div>
-            <Link to={'/user/home'}>
-                <button type="Button" className={"buttonEnter"}>Ingresar</button>
-            </Link>
-        </form> 
+            <p className={styles.copyrightText}>
+                Copyright © 2024 SextoSentido S.A.
+                Todos los derechos reservados
+            </p>
+        </div>
+        
     )
 }
 
